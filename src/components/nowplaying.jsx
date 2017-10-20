4de5eda3ab9ae1b5bf92655f8b13e4b9113a49e8
Parmvir Thind <parmvirthind@Parmvirs-MacBook-Pro.local>
2017-10-20 15:13:31 -0700

    adf

---
+++ src/components/nowplaying.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+class NowPlaying extends Component {
+  render () {
+    return (
+      <div>Current Song</div>
+    );
+  }
+}
--- src/containers/playlist.jsx
+++ src/containers/playlist.jsx
@@ -2,17 +2,17 @@
 
 import React, { Component } from 'react'
 import Button from 'button.jsx'
+import NowPlaying from 'nowplaying.jsx'
 
-class Playlist extends Component {
+ export default class Playlist extends Component {
   render() {
     return (
       <div>
-        <h6>Import</h6>
-        <p>Playlist 1</p>
-        <p>Playlist 2</p>
-        <p>Playlist 3</p>
+        < NowPlaying />
+        <p>Song 1</p>
+        <p>Song 2</p>
+        <p>Song 3</p>
         <button>Search</button>
-        <button>+ New</button>
       </div>
     )
   }
