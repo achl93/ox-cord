5e7045f0b608a2399c40f14ecf6dfab20b136fca
Jonathan Kalambay <jonnyk20@gmail.com>
2017-10-20 15:59:54 -0700

    installed packages

--- src/index.js
+++ src/index.js
@@ -6,17 +6,25 @@ import Host from './components/host';
 // import './index.css';
 // import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
 
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
+import reducers from './reducers';
+
+const createStoreWithMiddleware = applyMiddleware()(createStore);
+
 ReactDOM.render(
-  <BrowserRouter>
-    <Switch>
-      <Route path='/join' component={Join} />
-      <Route path='/host*' component={Host} />
-      <Route path='/' component={Home} />
-    </Switch>
-  </BrowserRouter>
+  <Provider store={createStoreWithMiddleware(reducers)}>
+    <BrowserRouter>
+      <Switch>
+        <Route path='/join' component={Join} />
+        <Route path='/host*' component={Host} />
+        <Route path='/' component={Home} />
+      </Switch>
+    </BrowserRouter>
+  </Provider>
   
   , document.getElementById('root'));
 registerServiceWorker();
