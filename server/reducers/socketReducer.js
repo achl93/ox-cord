286e7f41c4a6fcf3bcbb88ed50a59c3e54e2ecb8
Chad Garrett <chadgarrett_@hotmail.com>
2017-10-20 08:52:59 -0700

    Initial commit for socketReducer

---
+++ server/reducers/socketReducer.js
@@ -0,0 +1,9 @@
+const initialState = {  };
+
+function socketReducer(state = initialState, action) {
+  // For now, don't handle any actions
+  // and just return the state given to us.
+  return state
+}
+
+module.exports = { socketReducer }
