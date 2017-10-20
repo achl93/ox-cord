bc4587460c05d5b527a82cc75cf272f661fd6ffd
Jonathan Kalambay <jonnyk20@gmail.com>
2017-10-20 11:40:02 -0700

    routes work

--- src/index.js
+++ src/index.js
@@ -4,5 +4,14 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+ReactDOM.render(
+  <BrowserRouter>
+    <Switch>
+      <Route path='/app' component={App} />
+    </Switch>
+  </BrowserRouter>
+  
+  , document.getElementById('root'));
 registerServiceWorker();
