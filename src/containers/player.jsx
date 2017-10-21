0b21930f21a4a7eaf1362220dfac645afd70ce26
achl93 <andy_ch_009@hotmail.com>
2017-10-21 15:54:41 -0700

    songs now play dynamically based on songlist

--- src/containers/player.jsx
+++ src/containers/player.jsx
@@ -8,21 +8,27 @@ class Player extends Component {
     return (
       <div>
         <button onClick={() => this.onPlay()}>Play</button>
-        <button>Fast Forward</button>
+        <button onClick={() => this.onNext()}>Next</button>
       </div>
     )
   }
   onPlay(e) {
-    // Plays hardcoded song
-    // TODO: Play next song in playlist
-    this.props.playSong({"uris": ["spotify:track:4iV5W9uYEdYUVa79Axb7Rh"]});
+    // Currently plays songs dynamically, but from position 3 after the dummy data
+    // TODO: Plays next song in our playlist
+    this.props.playSong({"uris": [`spotify:track:${this.props.songs[3].id}`]});
+    console.log(this.props.songs[3].id);
+  }
+  onNext(e) {
+    // Plays next song in our virtual playlist
+    // this.props.playSong({});
   }
 }
 
 function mapStateToProps(state) {
   return {
     // TODO: Set state of nowPlaying to current song name
-    nowPlaying: state.nowPlaying
+    nowPlaying: state.nowPlaying,
+    songs: state.songs
   }
 }
 
