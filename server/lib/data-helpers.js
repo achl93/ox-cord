787c807ac5c46ddbc29f67854fbe1cfd6fbdcbf5
Chad Garrett <chadgarrett_@hotmail.com>
2017-10-21 10:41:12 -0700

    Renamed methods to more meaningful names

--- server/lib/data-helpers.js
+++ server/lib/data-helpers.js
@@ -2,18 +2,23 @@
 module.exports = function dataHelpers(db) {
   return {
 
-    createRoom: function (newTweet, callback) {
-      db.collection("rooms").insertOne(newTweet, (err, result) => {
+    createRoom: function (roomObj, callback) {
+      
+      db.collection("rooms").insertOne(roomObj, (err, result) => {
         callback(null, true);
       });
     },
 
-    getRoomData: function (room_id, callback) {
+    getRoomDataByID: function (room_id, callback) {
       db.collection("rooms").find({ "room_id": room_id }).toArray(callback);
     },
 
-    getRooms: function (callback) {
+    getActiveRooms: function (callback) {
       db.collection("rooms").find({"active": true}, {_id: 0, "room_id": 1, "name": 1}).toArray(callback);
+    },
+
+    getActiveRoomsNearby: function (coords, callback) {
+      // TO DO
     },
 
     getSongsFromRoomID: function(room_id, callback) {
