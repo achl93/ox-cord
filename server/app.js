452035a11d35306a798e1e51550d4d5cb59015a6
Chad Garrett <chadgarrett_@hotmail.com>
2017-10-20 19:19:06 -0700

    Added mongo dataHelpers and socket.on events for dealing with different responses from the socket server

--- server/app.js
+++ server/app.js
@@ -1,34 +1,74 @@
-const express   = require('express');
-const http      = require('http');
-const socketIo  = require('socket.io');
-const MongoDB   = require('mongodb').MongoClient
-const MongoURL  = 'mongodb://localhost:27017/oxcord';
+const express       = require('express');
+const http          = require('http');
+const socketIo      = require('socket.io');
+const MongoDB       = require('mongodb').MongoClient;
+const MongoURL      = 'mongodb://localhost:27017/oxcord';
 
-const app       = express();
-const server    = http.Server(app);
-const io        = socketIo(server);
+const app           = express();
+const server        = http.Server(app);
+const io            = socketIo(server);
+
+let numUsers = 0;
+let SHOW_DEBUG = true;
 
 MongoDB.connect(MongoURL, function(err, db) {
+  if (err) {
+    console.log(`Failed to connect to mongodb`);
+    throw err;
+  }
+  const dataHelpers   = require('./lib/data-helpers')(db);
   console.log("Connected successfully to MongoDB server");
-  db.close();
+
+  // dataHelpers.getRoomData('q6tubv3icueaamst4xw6h7go2', (err, data) => {
+  //   console.log(data);
+  // });
+
+  // dataHelpers.getRooms((err, data) => {
+  //   console.log(data);
+  // });
+
+  dataHelpers.getSongsFromRoomID('q6tubv3icueaamst4xw6h7go2', (err, data) => {
+    console.log(data);
+  });
 });
 
 io.on('connection', (socket) => {
-  const { id, name } = socket.decoded_token;
 
   socket.on('add-user', (username) => {
-
-  });
-
-  socket.on('join-room', (room) => {
-    //store.dispatch({ type: 'JOIN_ROOM', room_id: room });
-    socket.join(room);
+    if (SHOW_DEBUG) { console.log(' + add-user : ', username) }
+    socket.username = username;
+    ++numUsers;
     socket.broadcast.emit('user-joined', {
-
+      username: username,
+      numUsers: numUsers
     });
   });
 
-  console.log('Connected', id, name);
+  socket.on('join-room', (room) => {
+    if (SHOW_DEBUG) { console.log(' + join-room : ', room) }    
+    socket.join(room);
+    socket.broadcast.emit('user-joined', {
+      room_id: room
+    });
+  });
+
+  socket.on('set-tokens', (tokens) => {
+    if (SHOW_DEBUG) { console.log(' + set-tokens : ', tokens) }
+    // socket.broadcast sends to all EXCEPT client that sent
+    socket.broadcast.emit('tokens-set', {
+      something: true
+    });
+    // io.sockets.emit sends to ALL clients, including client that sent
+    io.sockets.emit('tokens-set', {
+      something_else: true
+    });
+  })
+
+  socket.on('set-geolocation', (geolocation) => {
+    if (SHOW_DEBUG) { console.log(' + set-geolocation : ', geolocation) }    
+  });
+
+  console.log('Connected successfully to Socket.IO');
 });
 
 server.listen(2017);
