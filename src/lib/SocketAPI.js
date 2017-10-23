8aac07bf799e2179ae08e4b5718043ab2e492b1b
Chad Garrett <chadgarrett_@hotmail.com>
2017-10-22 20:09:16 -0700

    Added multiple socket events which just console log the response

--- src/lib/SocketAPI.js
+++ src/lib/SocketAPI.js
@@ -1,12 +1,27 @@
 const socket = require('socket.io-client')('http://localhost:8888');
 
 socket.on('connect', () => {
-  console.log('connected to socket server')
-})
+  console.log('> Connected to Socket Server')
+});
 
 socket.on('room-created', (data) => {
-  console.log('ROOM CREATED!')
-  console.log(data)
+  console.log('> Room Created! ', data);
+});
+
+socket.on('user-joined', (data) => {
+  console.log('> Joined Room! ', data);
+});
+
+socket.on('vote-added', (data) => {
+  console.log('> Vote Added to Song! ', data);
+});
+
+socket.on('song-list-sent', (songs) => {
+  console.log('> Song List has been sent! ', songs);
+});
+
+socket.on('active-rooms-sent', (rooms) => {
+  console.log('> Active Rooms List has been sent! ', rooms);
 });
 
 module.exports = socket;
