8c69e8953282222190f6248dee891bfacea18e41
Chad Garrett <chadgarrett_@hotmail.com>
2017-10-24 13:08:47 -0700

    Initial UserSongList

---
+++ src/containers/UserSongList.jsx
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { Row, Col, ListGroup, ListGroupItem } from 'react-bootstrap';
+import UserSong from '../components/UserSong';
+
+class UserSongList extends Component {
+
+  renderSongs() {
+    if (Object.keys(this.props.songs).length !== 0) {
+      return this.props.songs.map((song) => {
+        return (
+          <UserSong key={song.id} song={song} user={this.props.user} />
+        )
+      }
+      );
+    } else {
+      return (<ListGroupItem> No songs added </ListGroupItem>)
+    }
+  }
+
+  render() {
+    return (
+      <Row>
+        <Col md={12}>
+          <Row>
+            <Col md={12}>
+              <h4 className='text-center'>Songs</h4>
+              <ListGroup >
+                {this.renderSongs()}
+              </ListGroup>
+            </Col>
+          </Row>
+        </Col>
+      </Row>
+    )
+  }
+
+}
+
+function mapStateToProps(state) {
+  return {
+    songs: state.songs,
+  }
+}
+
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({}, dispatch)
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(UserSongList);
