05ed0d70a378578ca767a7ba56bc270e69c4c345
Chad Garrett <chadgarrett_@hotmail.com>
2017-10-23 20:03:37 -0700

    Added some logic to redirect user to /user-playlist after joining room

--- src/components/JoinRoom.jsx
+++ src/components/JoinRoom.jsx
@@ -1,17 +1,45 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { joinRoom } from '../actions/index';
+import socket from '../lib/SocketAPI';
+import { Redirect } from 'react-router-dom';
 
-export default class JoinRoom extends Component {
+class JoinRoom extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      redirect: false
+    }
     this.sendToRoom = this.sendToRoom.bind(this);
   }
   sendToRoom() {
-    console.log("redirect to room");
+    console.log("> Redirect to Room");
     console.log(this.props.room_id);
+    this.props.joinRoom(this.props.room_id);
+    socket.on('user-joined', (room_id) => {
+      this.setState({ redirect: true });
+    });
   }
   render() {
-    return(
+    if (this.state.redirect) {
+      return <Redirect to={{pathname: '/user-playlist', state: { from: this.props.room_id } }} />;
+    }
+    return (
       <div><button onClick={this.sendToRoom}>{this.props.room_name}</button></div>
     )
   }
+
 }
+
+function mapStateToProps(state) {
+  return {
+    coords: state.coords
+  }
+}
+
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({ joinRoom }, dispatch)
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(JoinRoom);
