c0fe98d4a695a940f10fe51d29ef1d2f5dcc8116
Parmvir Thind <parmvirsthind@gmail.com>
2017-10-25 12:57:39 -0700

    added album cover to songs in queue and search result

--- src/components/SongSearchResult.jsx
+++ src/components/SongSearchResult.jsx
@@ -6,6 +6,7 @@ export default class SongSearchResult extends Component {
   render() {
     return (
       <ListGroupItem>
+      <img className = "album_cover" src={this.props.song.cover_art} />
       <h5>{ this.props.song.name } <Button bsClass='btn btn-sm btn-outline-success badge float-right addButton' onClick={()=>{this.onHandleClick()}}><i class="fa fa-plus" aria-hidden="true"></i></Button></h5> 
       <p class= "artist">{ this.props.song.artist }</p>
       </ListGroupItem>
