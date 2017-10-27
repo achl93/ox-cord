0789cd4992f1f39bd10e9bb46c3fe9b9f89db5b0
Parmvir Thind <parmvirsthind@gmail.com>
2017-10-27 14:18:57 -0700

    pushing changes so i can jump to a new branch

--- src/containers/Create.jsx
+++ src/containers/Create.jsx
@@ -24,7 +24,6 @@ class Create extends Component {
                         </div>
                       )
                     }
-
                   </Col>
                 </Row>
               )
