9d96b23e96323eaa9e20cebdd75d6ac1556ccc2d
Chad Garrett <chadgarrett_@hotmail.com>
2017-10-29 15:26:39 -0700

    Reenabled Geolocation

--- src/containers/Join.jsx
+++ src/containers/Join.jsx
@@ -14,22 +14,22 @@ class Join extends Component {
       flag: false
     }
   }
-  
-  componentDidMount() {
-    socket.emit('request-active-rooms');
-    socket.on('active-rooms-sent', (rooms) => {
-      this.setState({
-        rooms: rooms
-      });
-    });
-  }
+
+  // componentDidMount() {
+  //   socket.emit('request-active-rooms');
+  //   socket.on('active-rooms-sent', (rooms) => {
+  //     this.setState({
+  //       rooms: rooms
+  //     });
+  //   });
+  // }
 
   setLocation(pos) {
     this.props.getGeo({
       longitude: pos.coords.longitude,
       latitude: pos.coords.latitude
     });
-    this.setState({hasLocation: true});
+    this.setState({ hasLocation: true });
   }
 
   getGeolocation() {
@@ -39,36 +39,35 @@ class Join extends Component {
       navigator.geolocation.getCurrentPosition(this.setLocation.bind(this))
     }
   }
-  
+
   render() {
-    //this.getGeolocation();
-    //if (this.props.coords.longitude !== 0 && this.props.coords.latitude !== 0 && this.state.flag === false) {
-      //socket.emit('request-active-rooms-nearby', this.props.coords);
-      // socket.on('active-rooms-sent', (rooms) => {
-      //   this.setState({
-      //     rooms: rooms,
-      //     flag: true
-      //   });
-      // });
-      // return (
-      //   // this is pretty much not required
-      //   <i className="fa fa-circle-o-notch fa-spin fa-3x fa-fw"></i>
-      // )
-    //} else if (this.props.coords.longitude === 0 && this.props.coords.latitude === 0){
-      // return (
-      //   <i className="fa fa-circle-o-notch fa-spin fa-3x fa-fw"></i>
-      // )
-    //}
-      //else {
+    this.getGeolocation();
+    if (this.props.coords.longitude !== 0 && this.props.coords.latitude !== 0 && this.state.flag === false) {
+      socket.emit('request-active-rooms-nearby', this.props.coords);
+      socket.on('active-rooms-sent', (rooms) => {
+        this.setState({
+          rooms: rooms,
+          flag: true
+        });
+      });
       return (
-      <div>
-        Join a room!
-        { this.state.rooms.map((room) => {
-          return(<JoinRoom key={room._id} room_name={room.name} room_id={room.room_id} />);
-        }) }
-      </div>
+        // this is pretty much not required
+        <i className="fa fa-circle-o-notch fa-spin fa-3x fa-fw"></i>
       )
-    //}
+    } else if (this.props.coords.longitude === 0 && this.props.coords.latitude === 0) {
+      return (
+        <i className="fa fa-circle-o-notch fa-spin fa-3x fa-fw"></i>
+      )
+    } else {
+      return (
+        <div>
+          Join a room!
+        {this.state.rooms.map((room) => {
+            return (<JoinRoom key={room._id} room_name={room.name} room_id={room.room_id} />);
+          })}
+        </div>
+      )
+    }
     // <p>Longitude: {this.props.coords.longitude}</p>
     // <p>Latitude: {this.props.coords.latitude}</p>
   }
