64af0322a11b7e7ed7ccdaeec4cd8e46ea312824
Chad Garrett <chadgarrett_@hotmail.com>
2017-10-30 14:32:47 -0700

    Altered to a lower quality image (faster loading)

--- src/reducers/reducer_now_playing.jsx
+++ src/reducers/reducer_now_playing.jsx
@@ -1,6 +1,6 @@
 import { UPDATE_NOW_PLAYING } from '../actions/index';
 
-const initial = { id: 0, name: 'Nothing Playing Yet!', artist: 'Please wait for the Host to start the party!', cover_art: 'http://eskipaper.com/images/black-robot-bull-1.jpg', cover_background: 'http://eskipaper.com/images/black-robot-bull-1.jpg' }
+const initial = { id: 0, name: 'Nothing Playing Yet!', artist: 'Please wait for the Host to start the party!', cover_art: 'https://dncache-mauganscorp.netdna-ssl.com/thumbseg/1442/1442039-bigthumbnail.jpg', cover_background: 'https://dncache-mauganscorp.netdna-ssl.com/thumbseg/1442/1442039-bigthumbnail.jpg' }
 
 export default function(state = initial, action) {
   switch (action.type) {
