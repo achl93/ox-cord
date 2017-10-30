0e17b86f1e0574d5ee81fa222b542365939ad7eb
Chad Garrett <chadgarrett_@hotmail.com>
2017-10-30 13:54:50 -0700

    Added a remote img to server as a default if nothing is playing (an ox lol)

--- src/reducers/reducer_now_playing.jsx
+++ src/reducers/reducer_now_playing.jsx
@@ -1,6 +1,6 @@
 import { UPDATE_NOW_PLAYING } from '../actions/index';
 
-const initial = { id: 0, name: 'unknown' }
+const initial = { id: 0, name: 'unknown', cover_background: 'http://eskipaper.com/images/black-robot-bull-1.jpg' }
 
 export default function(state = initial, action) {
   switch (action.type) {
