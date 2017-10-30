0e87862dbe813a1dd41e5842dc0f1bff0c3d8dd3
Chad Garrett <chadgarrett_@hotmail.com>
2017-10-30 15:40:29 -0700

    Added space between Playlist Name and # of Tracks

--- src/components/PlaylistSearchResult.jsx
+++ src/components/PlaylistSearchResult.jsx
@@ -15,7 +15,7 @@ export default class PlaylistSearchResult extends Component {
        return <Redirect to='/playlist'/>;
      }
     return (
-      <ListGroupItem>{ this.props.playlist.name } -{ this.props.playlist.trackCount } track(s) <Button bsClass='btn btn-sm btn-outline-info float-right' onClick={()=>{this.onHandleClick()}}><i class="fa fa-download" aria-hidden="true"></i></Button></ListGroupItem>
+      <ListGroupItem>{ this.props.playlist.name } - { this.props.playlist.trackCount } track(s) <Button bsClass='btn btn-sm btn-outline-info float-right' onClick={()=>{this.onHandleClick()}}><i class="fa fa-download" aria-hidden="true"></i></Button></ListGroupItem>
     )
   }
 
