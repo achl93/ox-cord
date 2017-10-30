26aebacc108e0adbf3f35006f38c6d6ff1a82248
Chad Garrett <chadgarrett_@hotmail.com>
2017-10-30 14:38:32 -0700

    Added rounded borders class

--- src/components/Playlist.jsx
+++ src/components/Playlist.jsx
@@ -31,10 +31,10 @@ class Playlist extends Component {
     } else {
       return (
         <Row bsClass="mainCont col-md-8">
-          <Row bsClass=' row col-md-12 nowplayer px-3 pt-1 no-gutters'>
+          <Row bsClass=' row col-md-12 nowplayer rounded p-3 pt-1 no-gutters'>
             <NowPlaying />
           </Row>
-          <Row bsClass=' q_search justify-content-center row col-md-12 no-gutters '>
+          <Row bsClass=' q_search justify-content-center rounded row col-md-12 no-gutters '>
             <Row bsClass=' col-md-12 '>
               <Col md={12}>
                 <PlayerControls />
