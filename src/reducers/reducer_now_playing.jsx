6ed209eccd771b1b7943767f7d58430167d6d55e
Chad Garrett <chadgarrett_@hotmail.com>
2017-10-30 14:31:24 -0700

    Altered default name, artist and cover_background for when no song is currently playing

--- src/reducers/reducer_now_playing.jsx
+++ src/reducers/reducer_now_playing.jsx
@@ -1,6 +1,6 @@
 import { UPDATE_NOW_PLAYING } from '../actions/index';
 
-const initial = { id: 0, name: 'unknown', cover_background: 'http://eskipaper.com/images/black-robot-bull-1.jpg' }
+const initial = { id: 0, name: 'Nothing Playing Yet!', artist: 'Please wait for the Host to start the party!', cover_art: 'http://eskipaper.com/images/black-robot-bull-1.jpg', cover_background: 'http://eskipaper.com/images/black-robot-bull-1.jpg' }
 
 export default function(state = initial, action) {
   switch (action.type) {
