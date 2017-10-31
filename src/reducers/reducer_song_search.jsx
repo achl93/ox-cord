370c59126841e390c0f8202ec8e324df4c791d87
Chad Garrett <chadgarrett_@hotmail.com>
2017-10-30 18:01:44 -0700

    If search returns empty, at least produce a fake 'track' saying No Song / Artist Found

--- src/reducers/reducer_song_search.jsx
+++ src/reducers/reducer_song_search.jsx
@@ -54,6 +54,17 @@ const dummyResults = [
 export default function (state = dummyResults, action) {
   switch (action.type) {
     case SEARCH_SONGS:
+      // If search is empty, return an item that tells the user nothing found
+      if (action.payload.tracks === undefined) { 
+        return [{
+          id: 1,
+          name: 'No Song Found',
+          artist: 'No Artist Found',
+          votes: 0,
+          cover_art: 'https://cdn4.iconfinder.com/data/icons/cloudcon-v2/512/cloud-22-64.png',
+          playing: false
+        }]
+      }
       const newResults = action.payload.tracks.items.map(track => {
         return {
           id: track.id,
