2af51d83b266fe11197f393c9061a554bc47f26b
Parmvir Thind <parmvirsthind@gmail.com>
2017-11-01 18:30:45 -0700

    fixed dublicated code from conflict

--- src/containers/ExportPlaylist.jsx
+++ src/containers/ExportPlaylist.jsx
@@ -4,22 +4,22 @@ import { connect } from 'react-redux';
 import { Button } from 'react-bootstrap';
 import socket from '../lib/SocketAPI';
 import { bindActionCreators } from 'redux';
-import {remoteCreateRemotePlaylist} from '../actions/index';
+import { remoteCreateRemotePlaylist } from '../actions/index';
 
 class ExportPlaylist extends Component {
 
   today() {
     let today = new Date();
     let dd = today.getDate();
-    let mm = today.getMonth()+1;
+    let mm = today.getMonth() + 1;
     let yyyy = today.getFullYear();
 
-    if(dd<10) {
-        dd = '0'+dd
-    } 
-    if(mm<10) {
-        mm = '0'+mm
-    } 
+    if (dd < 10) {
+      dd = '0' + dd
+    }
+    if (mm < 10) {
+      mm = '0' + mm
+    }
     today = mm + '/' + dd + '/' + yyyy;
     return today;
   };
@@ -33,7 +33,7 @@ class ExportPlaylist extends Component {
         return `spotify:track:${song.id}`
       });
 
-      this.props.remoteCreateRemotePlaylist(this.props.user.id, songURI, this.today() );
+      this.props.remoteCreateRemotePlaylist(this.props.user.id, songURI, this.today());
     });
   };
 
@@ -54,21 +54,14 @@ class ExportPlaylist extends Component {
           </div>
           <div>
             <h6>Are you sure the party is over? All content will be deleted </h6>
-            <Button className="btn btn-danger mx-2">End Party</Button>
+            <Button onClick={() => { this.destroyRoom() }} className="btn btn-danger mx-2">End Party</Button>
           </div>
           <Link to='/playlist'>
             <div className='btn btn-outline-secondary btn-sm mt-3'><i className="fa fa-arrow-left" aria-hidden="true"></i> Back</div>
           </Link>
         </div>
-        <div>
-          <h6>Are you sure the party is over? All content will be deleted </h6>
-          <Button onClick={()=>{this.destroyRoom()}} className="btn btn-danger mx-2">End Party</Button>
-        </div>
-        <Link to='/playlist'>
-          <div className='btn btn-outline-secondary btn-sm mt-3'><i className="fa fa-arrow-left" aria-hidden="true"></i> Back</div>
-        </Link>
-      </div>
-    );
+      );
+    }
   }
 }
 
