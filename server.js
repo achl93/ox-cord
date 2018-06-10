04bfd53ca33acc2ae30d698758daeabdb1976c3b
Jonathan Kalambay <jonnyk20@gmail.com>
2018-06-10 00:20:33 -0700

    added code to allow cross origin message

--- server.js
+++ server.js
@@ -21,6 +21,16 @@ const distanceInKmBetweenEarthCoordinates = require('./src/lib/coordCalculator')
 let SHOW_DEBUG = true;
 let dataHelpers = require('./server-files/lib/data-helpers')
 
+
+app.use(function (req, res, next) {
+  res.setHeader(`Access-Control-Allow-Origin`, `*`);
+  res.setHeader(`Access-Control-Allow-Credentials`, `true`);
+  res.setHeader(`Access-Control-Allow-Methods`, `GET,HEAD,OPTIONS,POST,PUT,DELETE`);
+  res.setHeader(`Access-Control-Allow-Headers`, `Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers`);
+  res.setHeader(`Cache-Control`, `no-cache`);
+  next();
+});
+
 // using webpack-dev-server and middleware in development environment
 if (process.env.NODE_ENV !== 'production') {
   var webpackDevMiddleware = require('webpack-dev-middleware');
@@ -39,7 +49,7 @@ app.use(express.static(path.join(__dirname, 'dist')))
   .use(cookieParser())
   .use(spotifyRouteHelpers);
 
-app.get('*', function(request, response) {
+app.get('*', function (request, response) {
   response.sendFile(__dirname + '/dist/index.html')
 });
 
